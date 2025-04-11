7f53163c6aeaf8c3bc07db6e5329ccd9703894fb
Vit Dolinek <dolinekvit@gmail.com>
2025-04-11 23:51:16 +0200

    List all invoices

--- src/main/java/com/dolinek/fakturon/Invoice/Infrastructure/Mapper/InvoiceProductMapper.java
+++ src/main/java/com/dolinek/fakturon/Invoice/Infrastructure/Mapper/InvoiceProductMapper.java
@@ -15,31 +15,4 @@ import java.util.stream.Collectors;
 @Component
 public class InvoiceProductMapper {
 
-    public InvoiceProductDTO toDTO(InvoiceProduct invoiceProduct) {
-        InvoiceProductDTO dto = new InvoiceProductDTO();
-        if (invoiceProduct.getProduct() != null) {
-            dto.setProductId(invoiceProduct.getProduct().getId());
-        } else {
-            dto.setCustomName(invoiceProduct.getCustomName());
-        }
-        dto.setPrice(invoiceProduct.getPrice());
-        return dto;
-    }
-
-    public InvoiceProduct toEntity(InvoiceProductDTO dto, Invoice invoice, Product product) {
-        InvoiceProduct invoiceProduct = new InvoiceProduct(
-                invoice,
-                product,
-                dto.getCustomName(),
-                dto.getPrice(),
-                dto.getPriceTax(),
-                dto.getTaxAmount()
-        );
-
-        return invoiceProduct;
-    }
-
-    public List<InvoiceProductDTO> toDTOList(List<InvoiceProduct> invoiceProducts) {
-        return invoiceProducts.stream().map(this::toDTO).collect(Collectors.toList());
-    }
 }
--- src/main/java/com/dolinek/fakturon/Invoice/Web/Controller/InvoiceController.java
+++ src/main/java/com/dolinek/fakturon/Invoice/Web/Controller/InvoiceController.java
@@ -1,13 +1,13 @@
 package com.dolinek.fakturon.Invoice.Web.Controller;
 
+import com.dolinek.fakturon.Common.Domain.Model.PaginationResult;
+import com.dolinek.fakturon.Invoice.Domain.Model.Invoice;
 import com.dolinek.fakturon.Invoice.Infrastructure.Repository.InvoiceRepositoryImpl;
 import com.dolinek.fakturon.Invoice.Web.Dto.CreateUpdateInvoiceRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -18,6 +18,11 @@ public class InvoiceController {
 
     public InvoiceController(InvoiceRepositoryImpl repository) {
         this.repository = repository;
+    }
+
+    @GetMapping
+    public PaginationResult<Invoice> getInvoices(Pageable pageable) {
+        return this.repository.findAll(pageable);
     }
 
     @PostMapping
